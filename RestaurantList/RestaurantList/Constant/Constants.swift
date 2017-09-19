c5f35d6d8dd488df07cb03720620c8dce32909e4
woogii <jhsw0201@naver.com>
2017-09-19 23:50:54 +0200

    feat : Configure RestaurantInfoTableViewCell - Display distant information with formatted string - Display minimum cost information with formatted string - Implement setSelected, setHightlighted functions to display colors of UIViews

--- RestaurantList/RestaurantList/Constant/Constants.swift
+++ RestaurantList/RestaurantList/Constant/Constants.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 // MARK : - Constants
 
@@ -60,7 +61,29 @@ struct Constants {
   struct CellIDs {
     static let RestaurantInfoTableViewCell = "restaurantInfoTableViewCell"
   }
-  static let Open = "open"
-  static let OrderAhead = "orderAhead"
-  static let Closed = "closed"
+  // MARK : - Restaurant Information
+  struct RestaurantInfo {
+    static let Free = "FREE"
+    static let KiloMeter = "Km"
+    static let Min = "Min."
+    static let Open = "open"
+    static let OrderAhead = "orderAhead"
+    static let Closed = "closed"
+    static let DistanceFormat = "%.1f"
+  }
+  // MARK : - Colors
+  struct Colors {
+    static let LightBlue = UIColor(colorLiteralRed: 173/255.0,
+                                   green: 216/255.0, blue: 230/255.0, alpha: 0.7)
+  }
+  // MARK : - Images {
+  struct Images {
+    static let RestaurantOne   = "img_restaurant_one"
+    static let RestaurantTwo   = "img_restaurant_two"
+    static let RestaurantThree = "img_restaurant_three"
+    static let RestaurantFour  = "img_restaurant_four"
+    static let RestaurantFive  = "img_restaurant_five"
+    static let RestaurantSix   = "img_restaurant_six"
+  }
+  static let GermanLocale = "de_DE"
 }
--- RestaurantList/RestaurantList/View/RestaurantInfoTableViewCell.swift
+++ RestaurantList/RestaurantList/View/RestaurantInfoTableViewCell.swift
@@ -12,6 +12,8 @@ import Cosmos
 // MARK : - RestaurantInfoTableViewCell: UITableViewCell
 class RestaurantInfoTableViewCell: UITableViewCell {
   // MARK : - Property
+  @IBOutlet weak var distanceSeparatorView: UIView!
+  @IBOutlet weak var deliveryCostSeparatorView: UIView!
   @IBOutlet weak var restaurantImageView: UIImageView!
   @IBOutlet weak var openStatusLabel: UILabel!
   @IBOutlet weak var deliveryCostLabel: UILabel!
@@ -19,38 +21,81 @@ class RestaurantInfoTableViewCell: UITableViewCell {
   @IBOutlet weak var distanceLabel: UILabel!
   @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var openStatusSeparatorView: UIView!
-
   @IBOutlet var ratingAverageView: CosmosView!
-
+  private let restaurantImageNames: [String] = [Constants.Images.RestaurantOne,
+                                                Constants.Images.RestaurantTwo,
+                                                Constants.Images.RestaurantThree,
+                                                Constants.Images.RestaurantFour,
+                                                Constants.Images.RestaurantFive,
+                                                Constants.Images.RestaurantSix]
+  private let euroCurrencyFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    formatter.locale = Locale(identifier: Constants.GermanLocale)
+    return formatter
+  }()
   var restaurantInfo: Restaurant! {
     didSet {
       updateUI()
     }
   }
+  // MARK : - View Life Cycle
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    setCellBackgroundWhenTapped()
+  }
+  private func setCellBackgroundWhenTapped() {
+    let selectionColorView = UIView()
+    selectionColorView.backgroundColor = Constants.Colors.LightBlue
+    selectedBackgroundView = selectionColorView
+  }
   // MARK : - Update Cell UI
   private func updateUI() {
+    setRestaurantNameLabel()
     setRestaurantImageView()
-    setOpenStatusLabel()
+    setOpenStatusRelatedViews()
     setRatingAverageView()
+    setDeliveryCostLabel()
+    setDistanceLabel()
+    setMinimumCostLabel()
+    setAverageRatingView()
+  }
+  private func setRestaurantNameLabel() {
     nameLabel.text = restaurantInfo.name
-    restaurantImageView.image = #imageLiteral(resourceName: "img_restaurant_one")
-    openStatusLabel.text = restaurantInfo.status
-    deliveryCostLabel.text = "\(restaurantInfo.deliveryCosts)"
-    distanceLabel.text = "\(restaurantInfo.distance)"
-    minimumCostLabel.text = "\(restaurantInfo.minCost)"
-    ratingAverageView.rating = Double(restaurantInfo.ratingAverage)
+  }
+  private func setDistanceLabel() {
+    let distance = restaurantInfo.distance
+    let convertedDistance = String(format:Constants.RestaurantInfo.DistanceFormat, (Float)(distance)/1000) +
+      Constants.RestaurantInfo.KiloMeter
+    distanceLabel.text = convertedDistance
   }
   private func setRatingAverageView() {
     ratingAverageView.rating = restaurantInfo.ratingAverage
   }
   private func setRestaurantImageView() {
+    let randomIndex = Int(arc4random_uniform(UInt32(restaurantImageNames.count)))
+    restaurantImageView.image = UIImage(named:restaurantImageNames[randomIndex])
   }
-  private func setOpenStatusLabel() {
+  private func setAverageRatingView() {
+    ratingAverageView.rating = Double(restaurantInfo.ratingAverage)
+  }
+  private func setDeliveryCostLabel() {
+    if restaurantInfo.deliveryCosts == 0 {
+      deliveryCostLabel.text = Constants.RestaurantInfo.Free
+    } else {
+      deliveryCostLabel.text = euroCurrencyFormatter.string(from: NSNumber(value: restaurantInfo.deliveryCosts))
+    }
+  }
+  private func setMinimumCostLabel() {
+    minimumCostLabel.text = euroCurrencyFormatter.string(from: NSNumber(value: restaurantInfo.minCost))
+  }
+  private func setOpenStatusRelatedViews() {
     func displayOpenStatusViewsBasedOn(isHiddenValue: Bool) {
       openStatusLabel.isHidden = isHiddenValue
       openStatusSeparatorView.isHidden = isHiddenValue
     }
-    if restaurantInfo.status == Constants.Closed {
+    openStatusLabel.text = restaurantInfo.status
+    if restaurantInfo.status == Constants.RestaurantInfo.Closed {
       openStatusLabel.textColor = UIColor.red
       displayOpenStatusViewsBasedOn(isHiddenValue: false)
     } else {
@@ -58,5 +103,19 @@ class RestaurantInfoTableViewCell: UITableViewCell {
       displayOpenStatusViewsBasedOn(isHiddenValue: true)
     }
   }
-
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    let color = openStatusSeparatorView.backgroundColor
+    super.setSelected(isHighlighted, animated: animated)
+    setSeparatorViewsColors(color)
+  }
+  override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+    let color = openStatusSeparatorView.backgroundColor
+    super.setHighlighted(highlighted, animated: animated)
+    setSeparatorViewsColors(color)
+  }
+  private func setSeparatorViewsColors(_ color: UIColor?) {
+    openStatusSeparatorView.backgroundColor = color
+    distanceSeparatorView.backgroundColor = color
+    deliveryCostSeparatorView.backgroundColor = color
+  }
 }
