4049bde5bfd94679901fd6099de6a47745a99eb4
woogii <jhsw0201@naver.com>
2017-09-21 17:37:04 +0200

    feat : Create data model and managed object subclasses

---
+++ RestaurantList/RestaurantList/Model/FavoriteRestaurant+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteRestaurant+CoreDataClass.swift
+//  
+//
+//  Created by siwook on 2017. 9. 21..
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavoriteRestaurant)
+public class FavoriteRestaurant: NSManagedObject {
+
+}
